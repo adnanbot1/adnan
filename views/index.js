c55ca9d06f4b5074ca17dcc544d09c9e1c141aa8
adnanbot1 <102922097+adnanbot1@users.noreply.github.com>
2024-05-15 18:08:24 +0300

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const chat = document.querySelector('div.container-fluid');
   function addMsg(obj) {
-    const html = document.createElement('span');
+    const html = document.createElement('ar');
     html.className = 'msg';
     html.innerHTML = obj;
     chat.appendChild(html);
